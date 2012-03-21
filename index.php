81e38ed2d16e7ac83060ebce5cc0a832e875c71b
Aaron Wright <aaronmw@gmail.com>
2012-03-21 14:17:41 -0400

    Paging is dynamic; blah.com/gallery -> index.php?page=gallery

--- index.php
+++ index.php
@@ -49,100 +49,20 @@
 			</ul>
 		</nav>
 
-		<div id="page-wrapper" class="three-columns">
-			<section id="content">
-				<div id="thumbnails">
-					<?php
-						$courses = array (
-							'Book Design',
-							'Corporate ID',
-							'Communication Design',
-							'Design Workshop',
-							'Editorial Design',
-							'Independent Study',
-							'Information Design',
-							'Interactivity Design',
-							'Package Design',
-							'Time-Based Design',
-							'Typography'
-						);
-						$mediums = array (
-							'Interactive',
-							'Package',
-							'Print',
-							'Video'
-						);
+		<?php
+			$page_file = isset ($_GET['page']) ? 'pages/'.$_GET['page'].'.php' : 'pages/home.php';
 
-						for ($i = 0; $i < 105; $i++) {
-							$random_course = $courses[array_rand ($courses)];
-							$random_medium = $mediums[array_rand ($mediums)];
-							$random_number = rand (1, 5);
+			if (file_exists ($page_file)) {
+				require $page_file;
+			}
+			else
+			{
+				require 'pages/_error404.php';
+			}
+		?>
 
-							echo '
-								<div class="thumbnail" data-course="'.$random_course.'" data-medium="'.$random_medium.'" title="PROJECT NAME&lt;br&gt;Aaron Wright">
-									<a href="#">'.rand (1,5).'</a>
-								</div>
-							';
-						}
-					?>
-				</div>
-			</section>
+		<footer>
 
-			<section id="left" class="sidebar">
-				<h1>FRIENDS + FAMILY</h1>
-				<p>
-					APRIL 15<br>
-					6:30PM - 10:00PM
-				</p>
-
-				<h1>INDUSTRY NIGHT</h1>
-				<p>
-					APRIL 16<br>
-					6:30 - 10:00PM
-				</p>
-
-				<h1>DOORS OPEN</h1>
-				<p>
-					APRIL 16 &amp; 17<br>
-					10:00AM &mdash; 4:00PM
-				</p>
-			</section>
-
-			<section id="right" class="sidebar">
-				<h1>sort by</h1>
-				<ul>
-					<li><a href="#" id="sort_randomly">Random</a></li>
-					<li><a href="#" id="sort_by_medium">Medium</a></li>
-					<li><a href="#" id="sort_by_course">Course</a></li>
-				</ul>
-
-				<h1>filter by medium</h1>
-				<ul>
-					<li><a href="#" data-show-only-medium="all">All</a></li>
-					<?php
-						foreach ($mediums as $medium)
-						{
-							echo '
-								<li><a href="#" data-show-only-medium="'.$medium.'">'.$medium.'</a></li>
-							';
-						}
-					?>
-				</ul>
-
-				<h1>filter by course</h1>
-				<ul>
-					<li><a href="#" data-show-only-course="all">All</a></li>
-					<?php
-						foreach ($courses as $course)
-						{
-							echo '
-								<li><a href="#" data-show-only-course="'.$course.'">'.$course.'</a></li>
-							';
-						}
-					?>
-				</ul>
-			</section>
-		</div>
-
+		</footer>
 	</body>
 </html>
---
+++ pages/students.php
@@ -0,0 +1,68 @@
+<div id="page-wrapper" class="three-columns">
+	<section id="content">
+		<div id="thumbnails">
+			<?php
+				$letters = array ();
+				for ($i = 'a'; $i <= 'z'; $i++) {
+					$letters[] = $i;
+					if ($i == 'z') break;
+				}
+
+				echo 'here: <pre>'.print_r($letters, true).'</pre>';
+
+				for ($i = 0; $i < 105; $i++) {
+					$random_first_name = $letters[array_rand ($letters)];
+					$random_last_name = $letters[array_rand ($letters)];
+
+					echo '
+						<div class="thumbnail" data-first-name="'.$random_first_name.'" data-last-name="'.$random_last_name.'" title="'.$random_first_name.' '.$random_last_name.'">
+							<a href="#">'.$random_first_name.' '.$random_last_name.'</a>
+						</div>
+					';
+				}
+			?>
+		</div>
+	</section>
+
+	<section id="left" class="sidebar">
+		<h1>FRIENDS + FAMILY</h1>
+		<p>
+			APRIL 15<br>
+			6:30PM - 10:00PM
+		</p>
+
+		<h1>INDUSTRY NIGHT</h1>
+		<p>
+			APRIL 16<br>
+			6:30 - 10:00PM
+		</p>
+
+		<h1>DOORS OPEN</h1>
+		<p>
+			APRIL 16 &amp; 17<br>
+			10:00AM &mdash; 4:00PM
+		</p>
+	</section>
+
+	<section id="right" class="sidebar">
+		<h1>sort by</h1>
+		<ul>
+			<li><a href="#" id="sort_randomly">Random</a></li>
+			<li><a href="#" id="sort_by_medium">First Name</a></li>
+			<li><a href="#" id="sort_by_course">Last Name</a></li>
+		</ul>
+
+		<h1>filter by</h1>
+		<ul>
+			<li><a href="#" data-show-only-letter="all">All</a></li>
+			<?php
+				foreach ($letters as $letter)
+				{
+					echo '
+						<li><a href="#" data-show-only-letter="'.$letter.'">'.ucfirst ($letter).'</a></li>
+					';
+				}
+			?>
+		</ul>
+	</section>
+</div>
