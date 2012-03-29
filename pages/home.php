71e2cb54af9633a3835bd926f052dc6b6cadc135
Konstantin Sokhan <k@ksokhan.com>
2012-03-28 21:09:50 -0400

    Added new video and created the merch page

--- pages/home.php
+++ pages/home.php
@@ -1,5 +1,5 @@
 <div class="video">
-	<iframe src="http://player.vimeo.com/video/38615654?title=0&amp;byline=0&amp;portrait=0" width="600" height="335" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+	<iframe src="http://player.vimeo.com/video/38747706?title=0&amp;byline=0&amp;portrait=0" width="600" height="335" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 </div>
 
 <div id="info">
--- pages/video.php
+++ pages/video.php
@@ -1,6 +1,13 @@
 <div class="video">
-        <iframe src="http://player.vimeo.com/video/38615654?title=0&amp;byline=0&amp;portrait=0" width="600" height="335" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+	<div id="in-the-pique-of-time">
+		<iframe src="http://player.vimeo.com/video/38747706?title=0&amp;byline=0&amp;portrait=0" width="600" height="335" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+	</div>
+	<div id="pique-your-curiosity">
+		<iframe src="http://player.vimeo.com/video/38615654?title=0&amp;byline=0&amp;portrait=0" width="600" height="335" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+	</div>
 </div>
 
-<p style="text-align: center">Pique Your Curiosity.</p>
-
+<p style="text-align: center">
+	<a href="#in-the-pique-of-time" class="video-swapper">In the Pique of Time.</a>
+	<a href="#pique-your-curiosity" class="video-swapper">Pique Your Curiosity.</a>
+</p>
