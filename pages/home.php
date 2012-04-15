78575c0bcb30c0adefcc9c73c79fab23c4b429c6
Konstantin Sokhan <k@ksokhan.com>
2012-04-14 23:56:09 -0400

    new pique video

--- pages/home.php
+++ pages/home.php
@@ -1,5 +1,5 @@
 <div class="video">
-	<iframe src="http://player.vimeo.com/video/39519268?title=0&amp;byline=0&amp;portrait=0" width="600" height="335" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+	<iframe src="http://player.vimeo.com/video/40383039?title=0&amp;byline=0&amp;portrait=0" width="600" height="335" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 </div>
 
 <div id="info">
--- pages/video.php
+++ pages/video.php
@@ -1,4 +1,8 @@
 <div class="video">
+
+	<div id="get-your-pique-on">
+		<iframe src="http://player.vimeo.com/video/40383039?title=0&amp;byline=0&amp;portrait=0" width="600" height="335" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+	</div>
 	<div id="speak-the-pique">
 		<iframe src="http://player.vimeo.com/video/39519268?title=0&amp;byline=0&amp;portrait=0" width="600" height="335" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 	</div>
@@ -8,9 +12,11 @@
 	<div id="pique-your-curiosity">
 		<iframe src="http://player.vimeo.com/video/38615654?title=0&amp;byline=0&amp;portrait=0" width="600" height="335" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 	</div>
+
 </div>
 
 <p style="text-align: center">
+	<a href="#get-your-pique-on" class="video-swapper">Get Your Pique On.</a>
 	<a href="#speak-the-pique" class="video-swapper">Speak the Pique.</a>
 	<a href="#in-the-pique-of-time" class="video-swapper">In the Pique of Time.</a>
 	<a href="#pique-your-curiosity" class="video-swapper">Pique Your Curiosity.</a>
